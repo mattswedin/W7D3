ad3e2a4a75e05951eced3a6a286ca3a7a0f11f89
Matthew Swedin <matthew.swedin@gmail.comgit config --global user.email matthew.swedin@gmail.com>
2021-09-22 15:33:53 -0400

    push

--- GoalApp/spec/controllers/users_controller_spec.rb
+++ GoalApp/spec/controllers/users_controller_spec.rb
@@ -3,17 +3,36 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
   describe "GET #new" do
-    it "returns http success" do
+    it "it brings up the login page" do
+      allow(subject).to receive(:logged_in?)
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)
     end
   end
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    before :each do
+      # create(:user)
+      allow(subject).to receive(:current_user).and_return(User.last)
+    end
+    context "with valid params" do
+      it "creates a new user" do
+        POST :create, user: { username: "chicken", password: "ilovechicken" }
+        expect(User.last.username).to eq("chicken") 
+      end
+      it "redirects to goal index on success" do
+        POST :create, user: { username: "chicken", password: "ilovechicken" } 
+        expect(response).to redirect_to(goals_url)
+      end
+    end
+    context "with invalid params" do
+      it "renders the new template" do
+        POST :create, user: { username: "chicken" }
+        expect(response).to render_template(:new)
+      end
     end
   end
 
+
+  
 end
--- GoalApp/spec/factories/users.rb
+++ GoalApp/spec/factories/users.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :user do
-    username { "MyString" }
-    password_digest { "MyString" }
-    session_token { "MyString" }
+    username { "chicken" }
+    password { "ilovechicken" }
   end
 end
